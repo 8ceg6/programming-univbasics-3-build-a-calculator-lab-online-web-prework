238c19d93b840b813eabaf1cba2f5ad2e3c79f7e
cedric e goddard <8ceg6@comcast.net>
2019-08-09 19:35:43 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -13,13 +13,14 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-    retun num1 / num2
+    return num1 / num2
     num1 =50
     num2 =2
 end
 
 def multiplication(num1, num2)
-
+    return num1 * num2
+    num1
 end
 
 def modulo(num1, num2)
