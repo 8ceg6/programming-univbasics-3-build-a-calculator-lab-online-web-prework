7cc8fa4284579987feb9f592c950c07712696a5b
cedric e goddard <8ceg6@comcast.net>
2019-08-09 19:18:20 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,9 +1,5 @@
 def addition(num1, num2)
-    puts "Enter a number:"  
-    num1 = gets.chomp().to_f 
-    puts "enter another number"
-    num2 = gets.chomp().to_f
-    puts (num1 + num2)
+    puts 5 + 9
 end
 
 def subtraction(num1, num2)
