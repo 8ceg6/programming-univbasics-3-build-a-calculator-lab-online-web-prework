f77a8e36c1c6157f24300373ae8894f9c20f6c58
cedric e goddard <8ceg6@comcast.net>
2019-08-09 19:34:38 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -2,7 +2,7 @@ def addition(num1, num2)
     return num1+num2
     num1 =5
     num2 = 4
-    (addition(5,4))
+   
 end
 
 def subtraction(num1, num2)
@@ -13,7 +13,8 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-
+    retun num1 / num2
+    num1 =
 end
 
 def multiplication(num1, num2)
